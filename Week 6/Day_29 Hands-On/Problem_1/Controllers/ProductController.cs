f826e9f7878ef936cb1751d52e23448c42d2ba96
pogurunirupa <140179225+pogurunirupa@users.noreply.github.com>
2026-03-31 12:41:42 +0530

    Add files via upload

---
+++ Week 6/Day_29 Hands-On/Problem_1/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Temporary data (instead of DB)
+        static List<Product> products = new List<Product>();
+
+        // READ (Display all products)
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        // CREATE - GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE - POST
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                products.Add(p);
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        // EDIT - GET
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(x => x.ProductId == id);
+            return View(product);
+        }
+
+        // EDIT - POST
+        [HttpPost]
+        public IActionResult Edit(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = products.FirstOrDefault(x => x.ProductId == p.ProductId);
+
+                product.ProductName = p.ProductName;
+                product.Price = p.Price;
+                product.Category = p.Category;
+
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(x => x.ProductId == id);
+            products.Remove(product);
+            return RedirectToAction("Index");
+        }
+
+        // DETAILS
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(x => x.ProductId == id);
+            return View(product);
+        }
+    }
+}
---
+++ Week 6/Day_29 Hands-On/Problem_1/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApp.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Product ID is required")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be 5 to 15 characters")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Category must be 5 to 15 characters")]
+        public string Category { get; set; }
+    }
+}
