79873a9841680a65c913499878f0f9bd6644624f
pogurunirupa <140179225+pogurunirupa@users.noreply.github.com>
2026-03-31 12:43:41 +0530

    Add files via upload

---
+++ Week 7/Day_30 Hands-On/Problem_1/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContactApp.Models;
+
+namespace ContactApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        
+        private static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public ActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+        public ActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+            return View(contact);
+        }
+        public ActionResult AddContact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddContact(ContactInfo contactInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                contacts.Add(contactInfo);
+                return RedirectToAction("ShowContacts");
+            }
+
+            return View(contactInfo);
+        }
+    }
+}
---
+++ Week 7/Day_30 Hands-On/Problem_1/Models/ContactInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactApp.Models
+{
+    public class ContactInfo
+    {
+        [Required(ErrorMessage = "Contact Id is required")]
+        public int ContactId { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "First Name must be between 2 and 15 characters")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Last Name must be between 2 and 15 characters")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Company Name is required")]
+
+        public string CompanyName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter valid Email")]
+        public string EmailId { get; set; }
+        [Required(ErrorMessage = "Mobile number is required")]
+        [Range(1000000000, 9999999999, ErrorMessage = "Enter valid 10-digit mobile number")]
+        public long MobileNo { get; set; }
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+    }
+}
